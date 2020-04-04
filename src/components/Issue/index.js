c2eebd813569613ec4c00a1b5c66886c50b129e3
Abdulaziz Erel <azizerel69@gmail.com>
2020-04-04 23:19:01 +0300

    removed unused imports

--- src/components/Issue/index.js
+++ src/components/Issue/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import PropTypes from 'prop-types';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import EditIcon from '@material-ui/icons/Edit';
 import AddIcon from '@material-ui/icons/Add';
